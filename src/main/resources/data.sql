57e871030d5887640bc3ebd6abd2bdfe93d7775a
koroveshi <teoz.koroveshi@sisal.al>
2022-04-03 21:54:36 +0200

    first commit on data persist

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO class  (ID,CLASS_NAME) VALUES (2, 'Gym');
+INSERT INTO class  (ID,CLASS_NAME) VALUES (3, 'Literature');
+INSERT INTO class  (ID,CLASS_NAME) VALUES (4, 'Music');
+INSERT INTO class  (ID,CLASS_NAME) VALUES (5, 'Advanced Math');
