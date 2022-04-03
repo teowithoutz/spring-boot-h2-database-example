60997e9f6eeda1a5c3d1ebd1ae0b9222e167a878
koroveshi <teoz.koroveshi@sisal.al>
2022-04-03 22:41:05 +0200

    data persist update version 2

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,8 @@ INSERT INTO class  (ID,CLASS_NAME) VALUES (2, 'Gym');
 INSERT INTO class  (ID,CLASS_NAME) VALUES (3, 'Literature');
 INSERT INTO class  (ID,CLASS_NAME) VALUES (4, 'Music');
 INSERT INTO class  (ID,CLASS_NAME) VALUES (5, 'Advanced Math');
+
+INSERT INTO student (ID, AGE, EMAIL, NAME) VALUES (1,27,'marcus_aurelius@gmail.com', 'Marcus');
+INSERT INTO student (ID, AGE, EMAIL, NAME) VALUES (2,28,'marcus_brutus@gmail.com', 'Marcus');
+INSERT INTO student (ID, AGE, EMAIL, NAME) VALUES (3,31,'marc_anthony@gmail.com', 'Marc');
+INSERT INTO student (ID, AGE, EMAIL, NAME) VALUES (4,47,'julus_cesar@gmail.com', 'Julius');
